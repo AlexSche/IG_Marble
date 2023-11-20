4ca7547416ddf671e2ea426c301f393ed29cf0ad
AlexSche <Alexander.S_97@gmx.de>
2023-11-20 13:34:21 +0100

    working countdown

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,18 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private int timer = 60;
+    private TextMeshProUGUI timetext; 
+
+    void Start(){
+        timetext = GetComponent<TextMeshProUGUI>();
+        this.StartCoroutine("BeginCountdown");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator BeginCountdown() {
+        if (timer > 1) {
+            timer -= 1;
+            Debug.Log(timer);
+            timetext.text = timer.ToString();
+            yield return new WaitForSeconds(1);
+            this.StartCoroutine("BeginCountdown");
+        } else {
+            timetext.text = "Time is up!";
+            this.StopAllCoroutines();
+        }
     }
 }
