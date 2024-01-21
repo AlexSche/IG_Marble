3a0cd7dad74a2329dfb3c611896cd7d9d7bb3937
AlexSche <Alexander.S_97@gmx.de>
2024-01-21 19:44:40 +0100

    add settings menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    
+    void Start() {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
     public void StartGame() {
         SceneManager.LoadScene("Playground");
     }
 
     public void OpenSettings() {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
 
+    public void CloseSettings() {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
     }
 
     public void QuitGame() {
